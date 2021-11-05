1fa9864ef348901b2648a8741fe4ae6ba5007bef
tom-ikedy <ntntmyikd@gmail.com>
2021-11-05 01:57:24 +0000

    Add firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDAdlZNMU_OCfINzNq7Npl8nDmoEgmQ5_Q',
+  authDomain: 'tsukamoto-locations.firebaseapp.com',
+  projectId: 'tsukamoto-locations',
+  storageBucket: 'tsukamoto-locations.appspot.com',
+  messagingSenderId: '988568958924',
+  appId: '1:988568958924:web:1835e93338a980967e4f69',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
 
